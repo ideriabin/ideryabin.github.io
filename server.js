1ad3289770beb240b4267d5f78d1c870464b53dc
Igor Deryabin <igor@rodweb.ru>
2016-02-04 18:03:55 +0200

    Add Babel & fix browser-sync watchers from spawning around

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-var argv = require('yargs').argv;
-var paths = require('./package').config.paths;
-var bs = require('browser-sync').create();
-var exec = require('child_process').exec;
+import { argv } from 'yargs';
+import pkg from './package';
+import browserSync from 'browser-sync';
+import { exec } from 'child_process';
+
+const paths = pkg.config.paths;
+const bs = browserSync.create();
 
 function run(command) {
-  var proc = exec('npm run -s ' + command);
+  const proc = exec('npm run -s ' + command);
   proc.stdout.pipe(process.stdout);
   proc.stderr.pipe(process.stderr);
   return proc;
 }
+
+bs.watch(paths.css.glob, { ignoreInitial: true }, () => run('css:dev'));
+bs.watch(paths.html.glob, { ignoreInitial: true }, () => run('html'));
+bs.watch(paths.images.in, { ignoreInitial: true }, () => run('images'));
+bs.watch(paths.icons.in, { ignoreInitial: true }, () => run('icons'));
 
 bs.init({
   server: './public',
   files: paths.compiled,
   open: argv.open,
 });
-
-bs.watch(paths.css.glob, run.bind(null, 'css:dev'));
-bs.watch(paths.html.glob, run.bind(null, 'html'));
